5d0dc637b2f63f8c7e80916b8a274a8f6ecf9e7c
Orion Reed <orionreedt@gmail.com>
2024-03-31 14:36:11 -0700

    transpiles, probably has bugs

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  resolve: {
+    alias: {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",     // Must be below test-utils
+      "react/jsx-runtime": "preact/jsx-runtime"
+    },
+  }
 });
